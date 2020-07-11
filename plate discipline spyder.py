7892cc8cb08c73c25e38af9ff4240366749874a6
Allen-Ho-0302 <allenh0302@gmail.com>
2020-07-11 11:11:33 -0400

    With explanation

--- plate discipline spyder.py
+++ plate discipline spyder.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import pyodbc
 
+#connect with sql server, which already contains two tables from FanGraphs
+#1.https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=1000&type=8&season=2019&month=0&season1=2010&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=
+#2.https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=1000&type=5&season=2019&month=0&season1=2010&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=2010-01-01&enddate=2019-12-31
 sql_conn = pyodbc.connect('''DRIVER={ODBC Driver 13 for SQL Server};
                             SERVER=ALLENHO\MSSQLSERVER002;
                             DATABASE=Plate discipline and winning correlation;
                             Trusted_Connection=yes''') 
+
+#sql query to grab data, including players from 2010-2019 with over 1000 PA.
+#Define plate discipline as [Z-Swing%]-[O-Swing%])/[Swing%]
+#Also grab data from that corresponding player's BB%, K%, AVG, OBP, ISO, wOBA, wRC+, WAR/PA for that period
 query = '''
 SELECT p.name, ([Z-Swing%]-[O-Swing%])/[Swing%] as plated, [BB%], [K%], AVG, OBP, ISO, wOBA, [wRC+], (d.WAR/d.PA) as per_war
 FROM ['plate discipline 2010-2019 1000$'] p
@@ -12,14 +19,20 @@ JOIN ['dashboard stats 2010-2019 1000P$'] d
 on p.name = d.name
 order by WAR desc;
 '''
+
+#convert the data into dataframe
 df = pd.read_sql(query, sql_conn)
 
+#convert columns' type into string
 df.columns = df.columns.astype(str)
 
+#slice the data into only columns from plated(respresent plate discipline) to per_war(represent per game war)
 df_new = df.loc[:, 'plated':'per_war']
 
+#get the correlation table from df_new
 df_corr = df_new.corr()
 
+#slice the correlation table(df_corr) row from BB% to per game war
 df_corr_new = df_corr.loc['BB%':'per_war',:]
 
 
@@ -27,11 +40,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-print(df_corr_new.index)
-
-
 fig, ax = plt.subplots()
 
+#plot the correlation table as bar plot
 ax.bar(df_corr_new.index, df_corr_new['plated'])
 
 ax.set_xticklabels(df_corr_new.index, rotation=90)
@@ -43,120 +54,106 @@ plt.show()
 
 
 #---------------------------------------------------------------------------
-#import statsmodels.formula.api as smf
-#df['per_war2'] = df['per_war']**2
+#plot a scatter plot between plate discipline and per game war
+plt.plot(df_new['plated'], df_new['per_war'], 'o', alpha=0.2)
 
-#df['wRC'] = df['wRC+']
-#df['wRC2'] = df['wRC+']**2
-#df['wOBA2'] = df['wOBA']**2
-#df['ISO2'] = df['ISO']**2
-#df['OBP2'] = df['OBP']**2
-#df['AVG2'] = df['AVG']**2
-#df['K'] = df['K%']
-#df['K2'] = df['K%']**2
-#df['BB'] = df['BB%']
-#df['BB2'] = df['BB%']**2
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['per_war'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#results = smf.ols('plated~per_war + per_war2 + wRC + wRC2 + wOBA + wOBA2 + ISO + ISO2 + OBP + OBP2 + AVG + AVG2 + K + K2 + BB + BB2', data=df).fit()
-
-#print(results.params)
-
-#df = pd.DataFrame()
-#df['educ'] = np.linspace(0,20)
-#df['age'] = 30
-#df['educ2'] = df['educ']**2
-#df['age2'] = df['age']**2
-
-#pred = results.predict(df)
-#print(pred.head())
-
-
-
+plt.xlabel('plate discipline')
+plt.ylabel('per_game_war')
+plt.show()
 #----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['per_war'], 'o', alpha=0.2)
+##plot a scatter plot between plate discipline and wRC+
+plt.plot(df_new['plated'], df_new['wRC+'], 'o', alpha=0.2)
 
-#res = linregress(df_new['plated'], df_new['per_war'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['wRC+'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#plt.xlabel('plate discipline')
-#plt.ylabel('per_game_war')
-#plt.show()
+plt.xlabel('plate discipline')
+plt.ylabel('wRC+')
+plt.show()
+#-----------------------------------------------------------------------
+#plot a scatter plot between plate discipline and wOBA
+plt.plot(df_new['plated'], df_new['wOBA'], 'o', alpha=0.2)
+
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['wOBA'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
+
+plt.xlabel('plate discipline')
+plt.ylabel('wOBA')
+plt.show()
+#-----------------------------------------------------------------------
+#plot a scatter plot between plate discipline and ISO
+plt.plot(df_new['plated'], df_new['ISO'], 'o', alpha=0.2)
+
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['ISO'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
+
+plt.xlabel('plate discipline')
+plt.ylabel('ISO')
+plt.show()
 #----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['wRC+'], 'o', alpha=0.2)
+#plot a scatter plot between plate discipline and OBP
+plt.plot(df_new['plated'], df_new['OBP'], 'o', alpha=0.2)
 
-#res = linregress(df_new['plated'], df_new['wRC+'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['OBP'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#plt.xlabel('plate discipline')
-#plt.ylabel('wRC+')
-#plt.show()
-#-----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['wOBA'], 'o', alpha=0.2)
-
-#res = linregress(df_new['plated'], df_new['wOBA'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
-
-#plt.xlabel('plate discipline')
-#plt.ylabel('wOBA')
-#plt.show()
-#-----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['ISO'], 'o', alpha=0.2)
-
-#res = linregress(df_new['plated'], df_new['ISO'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
-
-#plt.xlabel('plate discipline')
-#plt.ylabel('ISO')
-#plt.show()
+plt.xlabel('plate discipline')
+plt.ylabel('OBP')
+plt.show()
 #----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['OBP'], 'o', alpha=0.2)
+#plot a scatter plot between plate discipline and AVG
+plt.plot(df_new['plated'], df_new['AVG'], 'o', alpha=0.2)
 
-#res = linregress(df_new['plated'], df_new['OBP'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['AVG'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#plt.xlabel('plate discipline')
-#plt.ylabel('OBP')
-#plt.show()
-#----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['AVG'], 'o', alpha=0.2)
-
-#res = linregress(df_new['plated'], df_new['AVG'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
-
-#plt.xlabel('plate discipline')
-#plt.ylabel('AVG')
-#plt.show()
+plt.xlabel('plate discipline')
+plt.ylabel('AVG')
+plt.show()
 #-----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['K%'], 'o', alpha=0.2)
+#plot a scatter plot between plate discipline and K%
+plt.plot(df_new['plated'], df_new['K%'], 'o', alpha=0.2)
 
-#res = linregress(df_new['plated'], df_new['K%'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['K%'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#plt.xlabel('plate discipline')
-#plt.ylabel('K%')
-#plt.show()
+plt.xlabel('plate discipline')
+plt.ylabel('K%')
+plt.show()
 #-----------------------------------------------------------------------
-#plt.plot(df_new['plated'], df_new['BB%'], 'o', alpha=0.2)
+#plot a scatter plot between plate discipline and BB%
+plt.plot(df_new['plated'], df_new['BB%'], 'o', alpha=0.2)
 
-#res = linregress(df_new['plated'], df_new['BB%'])
-#fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
-#fy = res.intercept + res.slope * fx
-#plt.plot(fx, fy)
+#plot the linear regression line 
+res = linregress(df_new['plated'], df_new['BB%'])
+fx = np.array([df_new['plated'].min(), df_new['plated'].max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy)
 
-#plt.xlabel('plate discipline')
-#plt.ylabel('BB%')
-#plt.show()
+plt.xlabel('plate discipline')
+plt.ylabel('BB%')
+plt.show()
